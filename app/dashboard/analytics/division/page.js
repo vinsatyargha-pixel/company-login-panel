c032e8ac994260aa115fecebbc8da4e95a831958
Alvin <vin.satyargha@gmail.com>
2026-02-24 02:11:22 +0700

    division page.js added

---
+++ app/dashboard/analytics/division/page.js
@@ -0,0 +1,9 @@
+'use client';
+
+export default function DivisionPage() {
+  return (
+    <div className="p-6 w-full min-h-screen bg-[#0B1A33] text-white">
+      <h1>Division Overview</h1>
+    </div>
+  );
+}
